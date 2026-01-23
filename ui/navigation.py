a8936954d6b86190c7fdf8d94161c93cb0affebb
Qzphs <86579079+Qzphs@users.noreply.github.com>
2026-01-23 13:57:55 +1300

    Shorten navigation text

--- ui/navigation.py
+++ ui/navigation.py
@@ -8,18 +8,18 @@ class Navigation(s.Frame):
     def __init__(self, parent: s.Screen):
         super().__init__(parent, 150, 200)
 
-        self.setup_button = s.TextLabel(self, "(setup screen)")
+        self.setup_button = s.TextLabel(self, "(setup)")
         self.setup_button.font = DEFAULT_FONT
         self.setup_button.place(150, 0, anchor=s.NE)
 
-        self.words_button = s.TextLabel(self, "(words screen)")
+        self.words_button = s.TextLabel(self, "(words)")
         self.words_button.font = DEFAULT_FONT
         self.words_button.place(150, 50, anchor=s.NE)
 
-        self.questions_button = s.TextLabel(self, "(questions screen)")
+        self.questions_button = s.TextLabel(self, "(questions)")
         self.questions_button.font = DEFAULT_FONT
         self.questions_button.place(150, 100, anchor=s.NE)
 
-        self.voting_button = s.TextLabel(self, "(voting screen)")
+        self.voting_button = s.TextLabel(self, "(voting)")
         self.voting_button.font = DEFAULT_FONT
         self.voting_button.place(150, 150, anchor=s.NE)
